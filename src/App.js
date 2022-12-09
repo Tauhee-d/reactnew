0fa21afb17e29840e0d8dcea34162ac696b88ea1
Tauhee-d <110287045+Tauhee-d@users.noreply.github.com>
2022-12-10 00:16:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Topbar from "./components/Topbar/Topbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Rooms from "./pages/Rooms/Room";
+import UserList from "./pages/UserList/UserList";
+import UserTable from "./pages/UserTable/UserTable";
+import UserDetail from "./pages/UserDetail/UserDetail";
+import TimeTemp from "./pages/TimeTemp/TimeTemp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Topbar title="fu@@d" />
+      <div className="containers">
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+          <Route path="/rooms">
+            <Rooms />
+          </Route>
+          <Route path="/patients">
+            <UserList />
+          </Route>
+          <Route path="/devices">
+            <UserTable />
+          </Route>
+          <Route path="/userDetail">
+            <UserDetail />
+          </Route>
+          <Route path="/doctors">
+            <TimeTemp />
+          </Route>
+
+          {/* <Route path="/MainRoom">
+      <MainRoom/>
+    </Route> */}
+        </Switch>
+        {/* <MainContainer/> */}
+        {/* <Dashboard/> */}
+      </div>
+    </Router>
+    // </Admin>
   );
 }
 
